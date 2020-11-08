56828e1155161111e8a3e1ec15ebc283ca98532f
Anton Kats <antonkats@gmail.com>
2020-11-08 09:24:16 -0400

    Creating area markers

--- src/pages/home/index.client.js
+++ src/pages/home/index.client.js
@@ -1,13 +1,55 @@
+// const CheapRuler = require('cheap-ruler').default
+
 const accessToken = "pk.eyJ1IjoiYW5raG1vciIsImEiOiJjaWZ4MTk4b2Eza2tqdTZrc2s3Y2x3Y3FuIn0.3emrqX3oCouiiHJwUXdFdg"
 
-var map = L.map('map-container');
+const map = L.map('map-container');
 map.locate({setView: true, enableHighAccuracy: true, maxZoom: 17});
 
 L.tileLayer.wms('https://api.mapbox.com/styles/v1/ankhmor/{id}/tiles/{tileSize}/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   id: 'ckgxxnwae12xc19plvvms6lys',
-  tileSize: 512,
-  zoomOffset: -1,
+  tileSize: 256,
+  zoomOffset: 0,
   accessToken
 }).addTo(map);
+
+
+let newLocationMarker;
+let newLocation;
+let ruler;
+
+let minRadius = 0
+let selectStage = 0;
+
+function setRadiusHandler(e) {
+  const radiusBoundary = e.latlng
+  // const distance = ruler.distance([newLocation.lat, newLocation.lng], [radiusBoundary.lat, radiusBoundary.lng]);
+  const distance = map.distance(newLocation, radiusBoundary);
+  if (distance > minRadius) {
+    newLocationMarker.setRadius(distance)
+  }
+}
+
+map.on('click', function(e){
+  if (selectStage === 1) {
+    selectStage = 0
+
+    map.off('mousemove', setRadiusHandler)
+  } else {
+    selectStage += 1
+
+    if (newLocationMarker) {
+      newLocationMarker.remove()
+    }
+
+    newLocation = e.latlng
+    // ruler = new CheapRuler(newLocation.lat, 'meters');
+    const radius = map.getZoomScale(18, map.getZoom()) * 10
+    minRadius = radius
+
+    newLocationMarker = new L.circle(newLocation, {radius}).addTo(map);
+
+    map.on('mousemove', setRadiusHandler)
+  }
+});
