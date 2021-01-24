20a4311b5276e6b01fc110bd5086f6974fe11f0d
Anton Kats <antonkats@gmail.com>
2021-01-24 15:04:19 -0400

    Tried filtering by density, better approach is count

--- src/model/plot/getInBounds.js
+++ src/model/plot/getInBounds.js
@@ -1,6 +1,8 @@
 import table                from "./table"
 import {castValuesToNumber} from "../../utils"
 
+const DENSITY_LIMITS = [0.01, 0.05, 0.3, 0.5]
+
 export default async function (bounds) {
     bounds = castValuesToNumber(bounds)
     const {south, north, east, west, diagonalLength} = bounds
@@ -10,18 +12,48 @@ export default async function (bounds) {
     * */
     const allRecords = await table.getAll()
     const maxRadius = diagonalLength / 3
+    const maxPlotArea = Math.pow(maxRadius * Math.PI, 2)
+    const mapArea = Math.pow(diagonalLength * Math.PI, 2)
 
-    const filteredRecords = allRecords.filter(r => {
-        r = castValuesToNumber(r)
+    const locationFiltered = allRecords.map(castValuesToNumber).filter(r => {
         return r.lat < north
-        && r.lat > south
-        && r.lng > west
-        && r.lng < east
-        && r.radius < maxRadius
+            && r.lat > south
+            && r.lng > west
+            && r.lng < east
+            && r.radius < maxRadius
     })
 
 
-    return filteredRecords.map(r => {
-        return {...r, sizeRank: Math.round(100 - r.radius / maxRadius * 100)}
+    // let maxRank = 0
+    const withSizeRank = locationFiltered.map(r => {
+        const area = Math.pow(r.radius * Math.PI, 2)
+        let sizeRank = Math.round(100 - Math.sqrt(area / maxPlotArea) * 100)
+        // if (maxRank < sizeRank) maxRank = sizeRank
+        if (sizeRank < 0) sizeRank = 0
+        return {...r, sizeRank, area}
     })
+
+    /*
+    * On the map we want to not overwhelm the user with too many markers, yet we still want to display a depth of sizes
+    * Thus we split all markers into 3 groups, and filter them based on density for each group
+    * */
+    // const groups = [maxRank * 0.75, maxRank * 0.5, maxRank * 0.25, 0]
+    const groups = [90, 75, 50, 0]
+    const preSecondPass = withSizeRank.map(r => {
+        return {...r, sizeGroup: groups.findIndex(g => r.sizeRank >= g), randomSortOrder: Math.random()}
+    }).sort((a, b) => a.randomSortOrder - b.randomSortOrder)
+
+    const groupAreaSums = [0, 0, 0, 0]
+    const groupDensities = [0, 0, 0, 0]
+    const final = preSecondPass.filter(r => {
+        const groupDensity = groupDensities[r.sizeGroup]
+        if (groupDensity <= DENSITY_LIMITS[r.sizeGroup]) {
+            groupAreaSums[r.sizeGroup] += r.area
+            groupDensities[r.sizeGroup] = groupAreaSums[r.sizeGroup] / mapArea
+            return true
+        }
+        return false
+    })
+
+    return final
 }
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,9 @@
-export function castValuesToNumber(bounds) {
-    return Object.fromEntries(Object.entries(bounds).map(([k, v]) => {
-        try {
-            return [k, Number(v)]
-        } catch {
+export function castValuesToNumber(obj) {
+    return Object.fromEntries(Object.entries(obj).map(([k, v]) => {
+        const n = Number(v)
+        if (!Number.isNaN(n)) {
+            return [k, n]
+        } else {
             return [k, v]
         }
     }))
